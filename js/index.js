039e62b613b65284a6ae936ac9e052a8eb124c2f
zhangjie <1148291351@qq.com>
2018-01-20 17:32:31 +0800

    three static page

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var m1 = angular.module("sanyibao",["ionic"]);
+	//初始化配置路由
+	m1.config(["$stateProvider",function($stateProvider){
+		$stateProvider.state("shouye",{
+			url:"/shouye",
+			templateUrl:"html/shouye.html"			
+		}).state("xiangmu",{
+			url:"/xiangmu",
+			templateUrl:"html/xiangmu.html",
+			controller:"xiangmu"
+		}).state("detail1",{
+			url:"/detail1/:id",
+			templateUrl:"html/detail1.html",
+			controller:"detail1"
+		})
+	}])
+			//初始化控制器
+	m1.controller("main",["$scope","$rootScope","$state","$http",function($scope,$rootScope,$state,$http){
+		$state.go("shouye");
+		$scope.doRefresh = function(){
+			$http({
+			method:"get",
+			url:"mock/detail.json",
+		}).success(function(data){
+			console.log(data.datas);
+			$scope.datas = data.datas
+		});
+			$scope.$broadcast('scroll.refreshComplete');
+		};
+			
+	}])
+	m1.controller("xiangmu",["$scope","$stateParams","$http",function($scope,$stateParams,$http){
+//		$scope.loadMore = function(){
+			$http({
+				method:"get",
+				url:"mock/detail.json",
+			}).success(function(data){
+				console.log(data.datas);
+				$scope.datas = data.datas
+//				$scope.$broadcast('scroll.infiniteScrollComplete');
+			});
+			
+//		}
+	}])
+	
+	m1.controller("detail1",["$scope","$stateParams","$http",function($scope,$stateParams,$http){
+		console.log($stateParams.id)
+		$http({
+			method:"get",
+			url:"mock/detail.json",
+		}).success(function(data){
+			var datas = data.datas
+			for(var i = 0;i < datas.length;i++){
+				if(datas[i].id == $stateParams.id){
+					$scope.data = datas[i] 					
+				}	
+			}
+			console.log($scope.data)
+		});
+		
+	}])
+	
